2e44269a5257184b20eb00999095b168b499eb94
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-10-09 11:48:15 -0700

    added definitions to config file

---
+++ cc_config.php
@@ -0,0 +1,8 @@
+<?php
+// Your couch database location (complete with http or https)
+define('COUCH_HOST','http://localhost');
+// Your database name, although most methods allow you to override this
+define('COUCH_DB','couchdb');
+// For methods that need authentication, your server setup may require this for more,
+// so change COUCH_HOST to this variable, ex: updates (by default) need authentication
+define('COUCH_SHOST','https://username:password@localhost');
