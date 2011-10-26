4b71e2d8b7119170b096ecc503ab70172c62edf2
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-10-26 15:39:49 -0700

    added reduce_emit option

--- cc_config.php
+++ cc_config.php
@@ -6,3 +6,5 @@ define('COUCH_DB','couchdb');
 // For methods that need authentication, your server setup may require this for more,
 // so change COUCH_HOST to this variable, ex: updates (by default) need authentication
 define('COUCH_SHOST','https://username:password@localhost');
+// Returns a reduced version of emit couch output for the view() method to show design document views
+define('REDUCE_EMIT',true);
